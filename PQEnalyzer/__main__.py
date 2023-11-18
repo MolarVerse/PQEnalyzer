39d0fb070d94609abb989054a7f33d9c18a6caa4
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2023-11-18 16:38:52 +0100

    Added argument parser to main

--- PQEnalyzer/__main__.py
+++ PQEnalyzer/__main__.py
@@ -2,13 +2,23 @@
 This is the main file of the PQEnalyzer project.
 """
 
+import argparse
+
+from .__version__ import __version__
 from .app import App
 
 def main():
     """
     The main function of the PQEnalyzer project.
     """
-    app = App()
+
+    parser = argparse.ArgumentParser(description="PQEnalyzer - MolarVerse")
+    parser.add_argument("filenames", metavar="filename", nargs="+", help="The name of the energy files to read the data from.")
+    parser.add_argument("--version", "-v", action="version", version=f"PQEnalyzer {__version__}")
+    
+    filenames = parser.parse_args().filenames
+
+    app = App(filenames)
     app.mainloop()
 
 if __name__ == "__main__":
